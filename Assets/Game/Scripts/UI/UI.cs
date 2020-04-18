40d419bbd0ee2701e32c3d068aab43cc343928f1
scsc <scscgit@users.noreply.github.com>
2020-04-18 09:55:06 +0200

    Downgraded Unity to 2018.4, added a basic Escape Menu

---
+++ Assets/Game/Scripts/UI/UI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts.UI
+{
+    public class UI : MonoBehaviour
+    {
+        public GameObject[] enableOnStart;
+        public GameObject[] toggleOnEscape;
+
+        void Start()
+        {
+            for (var i = 0; i < enableOnStart.Length; i++)
+            {
+                enableOnStart[i].SetActive(true);
+            }
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                for (var i = 0; i < toggleOnEscape.Length; i++)
+                {
+                    toggleOnEscape[i].SetActive(!toggleOnEscape[i].activeSelf);
+                }
+            }
+        }
+    }
+}
