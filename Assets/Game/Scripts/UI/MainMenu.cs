5ca19b58e2f6515ba7d96eaa4d21b82047a9e35d
scsc <scscgit@users.noreply.github.com>
2020-04-18 12:01:56 +0200

    Prepare basic Main Menu

---
+++ Assets/Game/Scripts/UI/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game.Scripts.UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        private void Awake()
+        {
+            // Android: don't sleep
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+            {
+                SelectCar();
+            }
+        }
+
+        public void SelectCar()
+        {
+            try
+            {
+                SceneManager.LoadScene("Game/Scenes/CarSelection");
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                //SceneManager.LoadScene(1);
+            }
+        }
+
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
