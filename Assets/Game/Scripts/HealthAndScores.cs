5627d382c1c05adaa9e56f52be9dc5ff26286a8e
thedetective456 <44827077+thedetective456@users.noreply.github.com>
2020-04-20 13:28:07 +0530

    Health and Scores!!!

---
+++ Assets/Game/Scripts/HealthAndScores.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthAndScores : MonoBehaviour
+{
+    public float health = 100f;
+    public int scores = 0;
+    private Rigidbody r;
+    public Slider s;
+    public Text scoreText;
+
+    private void Start() {
+        r = GetComponent<Rigidbody>();
+        s.value = 1f;
+    }
+
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        float v = r.velocity.magnitude;
+        if(!other.gameObject.GetComponent<Rigidbody>())
+        {
+            health -= v/20f;
+        }
+  
+        
+        if(other.gameObject.GetComponent<Rigidbody>())
+        {
+            float impactMag = Mathf.Abs(other.gameObject.GetComponent<Rigidbody>().velocity.magnitude * other.gameObject.GetComponent<Rigidbody>().mass - v * r.mass);                      
+            if(v > other.gameObject.GetComponent<Rigidbody>().velocity.magnitude)
+            {
+                if(other.gameObject.GetComponent<HealthAndScores>()) 
+                {
+                    other.gameObject.GetComponent<HealthAndScores>().health -= impactMag/10000f;
+                    scores += Mathf.RoundToInt(impactMag);
+                }
+                else health -= 1;                
+            }
+            else
+            {
+                health -= impactMag/10000f;
+            }
+        } 
+
+        
+        
+        s.value = health/100f;   
+        scoreText.text = scores.ToString();    
+    }
+}
---
+++ Assets/Game/Scripts/UI/Timer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private Text t;
+    public bool start;
+    public float tim;
+    Animator a;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        t = GetComponent<Text>();  
+        a = GetComponent<Animator>();      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(start)
+        { 
+            tim -= Time.deltaTime;
+            string m = Mathf.Floor((tim % 3600)/60).ToString();
+            string s = Mathf.Floor(tim % 60).ToString();
+            t.text = m + ":" + s;
+        }
+
+        a.SetBool("start", start);
+    }
+}
