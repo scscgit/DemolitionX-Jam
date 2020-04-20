da015dcd82b78a84258199984618d4d6f9470f86
scsc <scscgit@users.noreply.github.com>
2020-04-20 20:39:21 +0200

    Basic player name support
    Also opened Arena - "Arena.unity needs to be opened and resaved"

--- Assets/Game/Scripts/UI/MainMenu.cs
+++ Assets/Game/Scripts/UI/MainMenu.cs
@@ -1,15 +1,22 @@
 ﻿using System;
+using System.Linq;
 using Game.Scripts.Network;
 using Mirror;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
+using Random = System.Random;
 
 namespace Game.Scripts.UI
 {
     public class MainMenu : MonoBehaviour
     {
-        public GameObject menuPage;
+        public static string PlayerName;
+
+        [Header("Settings pages")] public GameObject menuPage;
         public GameObject settingsPage;
+        [Header("Text callbacks")] public InputField playerInput;
 
         private NetworkManager _networkManager;
 
@@ -24,6 +31,100 @@ namespace Game.Scripts.UI
 
             // Android: don't sleep
             Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+            // Player's name initialization
+            if (PlayerName == null)
+            {
+                var randomPlayerNames = new[]
+                {
+                    "Amazincar",
+                    "Auldkill",
+                    "BadSilly",
+                    "CarLumbar",
+                    "CornyCounty",
+                    "DamageDace",
+                    "DeathSilly",
+                    "DestroyDespot",
+                    "Destruction",
+                    "DestructionFree",
+                    "DestructionKill",
+                    "EarSilly",
+                    "KillCar",
+                    "Killersh",
+                    "KillSpice",
+                    "ProdigyKill",
+                    "BoboFunk",
+                    "Cipring",
+                    "Cucondov",
+                    "Cyteamar",
+                    "Deomanwo",
+                    "Depiles",
+                    "Derscgre",
+                    "DiaryMj",
+                    "Dragoney",
+                    "Ebroniah",
+                    "Finalco",
+                    "Gossipmere",
+                    "Hippogo",
+                    "HolyStand",
+                    "Interestod",
+                    "Iverlion",
+                    "JimQuey",
+                    "LastingMura",
+                    "LuckySaiyan",
+                    "LummoGurly",
+                    "Pinsten",
+                    "Proptexo",
+                    "Prosstar",
+                    "Reematte",
+                    "Riconintr",
+                    "Ronectis",
+                    "Rozbo",
+                    "ShmoeWicked",
+                    "Soltran",
+                    "Yourchno",
+                    "Adygenco",
+                    "Appelered",
+                    "Beautyra",
+                    "BlinkSlim",
+                    "Enjoyerca",
+                    "Exoticitec",
+                    "Facellon",
+                    "Flamespier",
+                    "Gielineba",
+                    "Glimmercoun",
+                    "Humandson",
+                    "Interela",
+                    "KhadZeether",
+                    "KingAni",
+                    "Malleonia",
+                    "Mamade",
+                    "Mooneyes",
+                    "Nycci",
+                    "Pflowapri",
+                    "QueyChiquita",
+                    "SellProud",
+                    "Serentma",
+                    "Sliprope",
+                    "Stroonsentu",
+                    "SumoKnight",
+                    "TaryBago",
+                    "Tenaxinet",
+                    "TheborgStronger",
+                    "Thrillwood",
+                    "WetPunk",
+                };
+                if (randomPlayerNames.Any(test => test.Length > playerInput.characterLimit))
+                {
+                    throw new Exception("playerInput.characterLimit exceeded by one of the default names");
+                }
+
+                ChangePlayerName(randomPlayerNames[new Random().Next(randomPlayerNames.Length)]);
+            }
+            else
+            {
+                ChangePlayerName(PlayerName);
+            }
         }
 
         private void Update()
@@ -45,6 +146,15 @@ namespace Game.Scripts.UI
         public void ConnectClient()
         {
             _networkManager.StartClient();
+        }
+
+        /// <summary>
+        /// Player name change, compatible with whichever the side it was called from from (text-box or code).
+        /// </summary>
+        public void ChangePlayerName(string playerName)
+        {
+            PlayerName = playerName;
+            playerInput.text = playerName;
         }
 
         [Obsolete]
