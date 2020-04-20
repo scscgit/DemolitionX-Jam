1d0d0994f8c01985aa54644e800cd6cc0ff73bb7
anonymous-T <anonymous-T>
2020-04-21 04:01:14 +0500

    time..

--- Assets/Game/Scripts/UI/Timer.cs
+++ Assets/Game/Scripts/UI/Timer.cs
@@ -1,18 +1,26 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     private Text t;
     public bool start;
-    public float tim;
+    public float mins;
+    public float sec;
+    public float time;
     Animator a;
     
     // Start is called before the first frame update
     void Awake()
     {
         t = GetComponent<Text>();  
-        a = GetComponent<Animator>();      
+        a = GetComponent<Animator>();
+        OnReset();
+    }
+
+    public void OnReset()
+    {
+        time = (sec + (mins * 60));
     }
 
     // Update is called once per frame
@@ -20,12 +28,19 @@ public class Timer : MonoBehaviour
     {
         if(start)
         { 
-            tim -= Time.deltaTime;
-            string m = Mathf.Floor((tim % 3600)/60).ToString();
-            string s = Mathf.Floor(tim % 60).ToString();
+            time -= Time.deltaTime;
+            string m = Mathf.Floor((time /60) % 60).ToString();
+            string s = Mathf.Floor(time % 60).ToString();
             t.text = m + ":" + s;
         }
+        if (time < 0)
+            ResetGame();
 
         a.SetBool("start", start);
+    }
+
+    public void ResetGame()
+    {
+        
     }
 }
